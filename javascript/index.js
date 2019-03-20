2933de7fe10b3c18183816cc59257c848f0d7d13
neo <18428193429@163.com>
2019-03-20 17:34:51 +0800

    modify page2

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,13 @@
 $(window).load(function () {
 
     $(".page2_content1 .page2_content1_img").hover(function () {
-        $($(this).children("img").eq(1)).animate({opacity:1},100);
+        $($(this).children("div")).animate({opacity:1},100);
     },function () {
-        $($(this).children("img").eq(1)).animate({opacity:0},100);
+        $($(this).children("div")).animate({opacity:0},100);
     });
-    $(".page2_content1 img").not($(".page2_content1_img_mask")).click(function () {
-        console.log($(this).attr("src"));
-        $("#myModal img").attr("src",($(this).attr("src")));
+    $(".page2_content1_img_mask a").click(function () {
+        console.log($(this).parent().siblings("img"));
+        $("#myModal img").attr("src",($(this).attr("img-src")));
     });
 
 
